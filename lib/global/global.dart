9f43bcfb62785e629edf8c14022d136ec69e571d
ttoghere <59641232+ttoghere@users.noreply.github.com>
2023-11-09 10:49:38 +0300

    .ç

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -4,4 +4,4 @@ import 'package:tunctaxidriver/models/user_model.dart';
 final FirebaseAuth fAuth = FirebaseAuth.instance;
 User? currentFirebaseUser;
 UserModel? userModelCurrentInfo;
-String mapKey = "AIzaSyDolKIcAQumeiQ6nGU0tQxtoq-1emSelbM";
+String mapKey = "AIzaSyCKDYvMGPo386lnpGr11t2bIox7BE7SHGM";
